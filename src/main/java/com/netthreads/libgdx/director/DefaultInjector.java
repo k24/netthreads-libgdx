5e415904cf49f864566594d982202cade5af8df0
k24 <nishikei.9@gmail.com>
2017-01-02 22:29:23 +0900

    Cache instances in DefaultInjector

--- src/main/java/com/netthreads/libgdx/director/DefaultInjector.java
+++ src/main/java/com/netthreads/libgdx/director/DefaultInjector.java
@@ -14,24 +14,27 @@ import java.util.HashMap;
  */
 public class DefaultInjector implements Injector {
     private final HashMap<Class<?>, Creator<?>> creatorMap = new HashMap<Class<?>, Creator<?>>();
+    private final HashMap<Class<?>, Object> instanceMap = new HashMap<Class<?>, Object>();
 
     public DefaultInjector withInstallDefault() {
-        return (DefaultInjector) this
-                .bind(Director.class, new SimpleCreator<Director>())
-                .bind(SoundAssetManager.class, new Creator<SoundAssetManager>() {
-                    @Override
-                    public SoundAssetManager createInstance(Class<SoundAssetManager> type) {
-                        return new SoundAssetManager(getInstance(SoundDefinitions.class));
-                    }
-                })
-                .bind(SoundCache.class, new SimpleCreator<SoundCache>())
-                .bind(TextureAssetManager.class, new Creator<TextureAssetManager>() {
-                    @Override
-                    public TextureAssetManager createInstance(Class<TextureAssetManager> type) {
-                        return new TextureAssetManager(getInstance(TextureDefinitions.class));
-                    }
-                })
-                .bind(TextureCache.class, new SimpleCreator<TextureCache>());
+        synchronized (creatorMap) {
+            return (DefaultInjector) this
+                    .bind(Director.class, new SimpleCreator<Director>())
+                    .bind(SoundAssetManager.class, new Creator<SoundAssetManager>() {
+                        @Override
+                        public SoundAssetManager createInstance(Class<SoundAssetManager> type) {
+                            return new SoundAssetManager(getInstance(SoundDefinitions.class));
+                        }
+                    })
+                    .bind(SoundCache.class, new SimpleCreator<SoundCache>())
+                    .bind(TextureAssetManager.class, new Creator<TextureAssetManager>() {
+                        @Override
+                        public TextureAssetManager createInstance(Class<TextureAssetManager> type) {
+                            return new TextureAssetManager(getInstance(TextureDefinitions.class));
+                        }
+                    })
+                    .bind(TextureCache.class, new SimpleCreator<TextureCache>());
+        }
     }
 
     @Override
@@ -43,9 +46,17 @@ public class DefaultInjector implements Injector {
     @SuppressWarnings("unchecked")
     @Override
     public <T> T getInstance(Class<T> type) {
-        Creator<T> creator = (Creator<T>) creatorMap.get(type);
-        if (creator == null) throw new IllegalStateException("'" + type.getName() + "' must be bound before getting");
-        return creator.createInstance(type);
+        synchronized (creatorMap) {
+            T instance = (T) instanceMap.get(type);
+            if (instance == null) {
+                Creator<T> creator = (Creator<T>) creatorMap.get(type);
+                if (creator == null)
+                    throw new IllegalStateException("'" + type.getName() + "' must be bound before getting");
+                instance = creator.createInstance(type);
+                instanceMap.put(type, instance);
+            }
+            return instance;
+        }
     }
 
     public static class SimpleCreator<T> implements Creator<T> {
